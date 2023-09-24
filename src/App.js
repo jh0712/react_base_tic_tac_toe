b636beeccde1684fdbdb07e328257011f5033eff
linkaihua <jhbelieve0712@gmail.com>
2023-09-25 00:25:14 +0800

    add square block

--- src/App.js
+++ src/App.js
@@ -1,4 +1,32 @@
-import React  from 'react';
-export default function Square() {
-  return <button className="square">X</button>;
+import React from 'react';
+import { useState } from 'react';
+
+export default function Board() {
+    return (
+        <div>
+            <div className="board-row">
+                <Square ></Square>
+                <Square ></Square>
+                <Square ></Square>
+            </div>
+            <div className="board-row">
+                <Square ></Square>
+                <Square ></Square>
+                <Square ></Square>
+            </div>
+            <div className="board-row">
+                <Square ></Square>
+                <Square ></Square>
+                <Square ></Square>
+            </div>
+        </div>
+    );
 }
+function Square() {
+    const [value, setValue] = useState(null);
+    function handleClick() {
+        setValue('X')
+    }
+    return <button className="square" onClick={handleClick}>{value}</button>;
+}
+
