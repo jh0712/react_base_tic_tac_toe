1f191a163e8a39142b6fa7163947d80baefb61e2
linkaihua <jhbelieve0712@gmail.com>
2023-09-29 16:16:20 +0800

    add edit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { useState } from 'react';
 
 export default function Board() {
+    const [squares,setSquare] = useState(Array(9).fill(null))
+    function handleClick(i) {
+        // const nextSquares = squares.slice();
+        // nextSquares[i] = "X";
+        // setSquare(nextSquares);
+        squares[i] = 'X';
+        setSquare(squares);
+    }
     return (
         <div>
             <div className="board-row">
-                <Square ></Square>
-                <Square ></Square>
-                <Square ></Square>
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
+                <Square value={squares[1]}></Square>
+                <Square value={squares[2]} ></Square>
             </div>
             <div className="board-row">
                 <Square ></Square>
@@ -22,11 +30,12 @@ export default function Board() {
         </div>
     );
 }
-function Square() {
-    const [value, setValue] = useState(null);
-    function handleClick() {
-        setValue('X')
-    }
-    return <button className="square" onClick={handleClick}>{value}</button>;
+function Square({value ,onSquareClick}) {
+    // const [value, setValue] = useState(null);
+    // function handleClick() {
+    //     setValue('X')
+    // }
+    return <button className="square" onClick={onSquareClick}>{value}</button>;
+    // return <button className="square" >{value}</button>;
 }
 
